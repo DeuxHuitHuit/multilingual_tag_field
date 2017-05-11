d1c2ae7e7612162a638f259ee707cd714ae2ad73
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-11 13:11:45 -0400

    Place checkbox before string in label

--- extension.driver.php
+++ extension.driver.php
@@ -103,7 +103,7 @@
 			$group->appendChild(new XMLElement('legend', __(MTF_NAME)));
 
 			$label = Widget::Label(__('Consolidate entry data'));
-			$label->appendChild(Widget::Input('settings['.MTF_GROUP.'][consolidate]', 'yes', 'checkbox', array('checked' => 'checked')));
+			$label->prependChild(Widget::Input('settings['.MTF_GROUP.'][consolidate]', 'yes', 'checkbox', array('checked' => 'checked')));
 			$group->appendChild($label);
 			$group->appendChild(new XMLElement('p', __('Check this field if you want to consolidate database by <b>keeping</b> entry values of removed/old Language Driver language codes. Entry values of current language codes will not be affected.'), array('class' => 'help')));
 
